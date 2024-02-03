aa918781c5353b1d92daea59f9fdf9949132924c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-03 09:10:47 -0500

    feat: add  load page by user use case to be called by store

---
+++ src/users/use-cases/load-users-by-page.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {Number} page 
+ */
+export const loadUsersByPage = async (page = 1) => {
+    const url = `${import.meta.env.VITE_BASE_URL}/users?_page=${page}`
+    const res = await fetch(url)
+    const data = await res.json()
+
+    console.log(data)
+}
