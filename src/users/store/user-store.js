8eb58fb2afaba22ca2d15af8a6fadab996b52347
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-03 09:11:19 -0500

    feat: implement loadNextPage
    call loadUsersByPage use case

--- src/users/store/user-store.js
+++ src/users/store/user-store.js
@@ -1,10 +1,12 @@
+import { loadUsersByPage } from "../use-cases/load-users-by-page"
+
 const state = {
     currentPage: 0,
     users: []
 }
 
 const loadNextPage = async () => {
-    throw new Error('Not implemented')
+    await loadUsersByPage(state.currentPage + 1)
 }
 const loadPreviousPage = async () => {
     throw new Error('Not implemented')
