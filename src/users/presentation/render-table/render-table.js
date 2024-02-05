43a32f9e9347b7f5ea1b55935fc0b71e11fb7cf3
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-04 20:17:01 -0500

    feat: add table render implementation

---
+++ src/users/presentation/render-table/render-table.js
@@ -0,0 +1,60 @@
+import usersStore from '../../store/users-store'
+import './render-table.css'
+
+let table
+
+const createTable = () => {
+    const table = document.createElement('table')
+    const tableHeaders = document.createElement('thead')
+    tableHeaders.innerHTML = `
+        <tr>
+            <th>#ID</th>
+            <th>Balance</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Active</th>
+            <th>Actions</th>
+        </tr>
+    `
+
+    const tableBody = document.createElement('tbody')
+    table.append(tableHeaders, tableBody)
+
+    return table
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderTable = (element) => {
+    const users = usersStore.getUsers()
+
+    if (!table) {
+        table = createTable()
+        element.append(table)
+
+        // TODO: Listeners a la tabla
+    }
+
+    let tableHTML = ''
+
+    users.forEach(user => {
+        tableHTML += `
+            <tr>
+                <td>${user.id}</td>
+                <td>${user.balance}</td>
+                <td>${user.firstName}</td>
+                <td>${user.lastName}</td>
+                <td>${user.isActive}</td>
+                <td>
+                    <a href="#" data-id="${user.id}">Select</a>
+                    |
+                    <a href="#" data-id="${user.id}">Delete</a>
+                </td>
+            </tr>
+        `
+    })
+
+    table.querySelector('tbody').innerHTML = tableHTML
+}
