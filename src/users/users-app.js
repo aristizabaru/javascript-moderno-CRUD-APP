4e00883b33cebddc11e38e460b4931b63d8342d7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-05 15:43:56 -0500

    feat: add callback to modal
    saves or updates user from form

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -3,6 +3,7 @@ import { renderButtons } from "./presentation/render-buttons/render-buttons"
 import { renderModal } from "./presentation/render-modal/render-modal"
 import { renderTable } from "./presentation/render-table/render-table"
 import usersStore from "./store/users-store"
+import { saveUser } from "./use-cases/save-user"
 
 /**
  * 
@@ -16,5 +17,9 @@ export const UserApp = async (element) => {
     renderTable(element)
     renderButtons(element)
     renderAddButton(element)
-    renderModal(element)
+    renderModal(element, async (userLike) => {
+        const user = await saveUser(userLike)
+        usersStore.onUserChanged(user)
+        renderTable()
+    })
 }
