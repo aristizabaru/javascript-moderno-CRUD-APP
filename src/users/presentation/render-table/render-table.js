9202ff815770c47823b93ff0ea6177df57b8b6f9
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-05 15:47:40 -0500

    feat: add listener to delete and select buttons

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,6 @@
 import usersStore from '../../store/users-store'
+import { deleteUserById } from '../../use-cases/delete-user-by-id'
+import { showModal } from '../render-modal/render-modal'
 import './render-table.css'
 
 let table
@@ -25,6 +27,35 @@ const createTable = () => {
 
 /**
  * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = (event) => {
+    const element = event.target.closest('.select-user')
+    if (!element) return
+
+    const id = element.getAttribute('data-id')
+    showModal(id)
+}
+
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableDeleteListener = async (event) => {
+    const element = event.target.closest('.delete-user')
+    if (!element) return
+
+    const id = element.getAttribute('data-id')
+    await deleteUserById(id)
+    await usersStore.reloadPage()
+
+    document.querySelector('#current-page').innerText = usersStore.getCurrentPage()
+    renderTable()
+
+}
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
 export const renderTable = (element) => {
@@ -34,7 +65,8 @@ export const renderTable = (element) => {
         table = createTable()
         element.append(table)
 
-        // TODO: Listeners a la tabla
+        table.addEventListener('click', tableSelectListener)
+        table.addEventListener('click', tableDeleteListener)
     }
 
     let tableHTML = ''
@@ -48,9 +80,9 @@ export const renderTable = (element) => {
                 <td>${user.lastName}</td>
                 <td>${user.isActive}</td>
                 <td>
-                    <a href="#" data-id="${user.id}">Select</a>
+                    <a href="#" class="select-user" data-id="${user.id}">Select</a>
                     |
-                    <a href="#" data-id="${user.id}">Delete</a>
+                    <a href="#" class="delete-user" data-id="${user.id}">Delete</a>
                 </td>
             </tr>
         `
