326970522c0ec1bbc5b8a1f17abaf17e10b806a9
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-05 15:47:16 -0500

    feat: add form actions from send button

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,12 +1,22 @@
 import modalHTML from './render-modal.html?raw'
 import './render-modal.css'
+import { User } from '../../models/user'
+import { getUserById } from '../../use-cases/get-user-by-id'
 
 let modal, form
+let loadedUser = {}
 
-
-// TODO: cargar usuario por id
-export const showModal = () => {
+/**
+ * 
+ * @param {String|Number} id 
+ */
+export const showModal = async (id) => {
     modal?.classList.remove('hide-modal')
+
+    if (!id) return
+
+    const user = await getUserById(id)
+    setFormValues(user)
 }
 
 export const hideModal = () => {
@@ -16,9 +26,23 @@ export const hideModal = () => {
 
 /**
  * 
- * @param {HTMLDivElement} element 
+ * @param {User} user 
  */
-export const renderModal = (element) => {
+const setFormValues = (user) => {
+    form.querySelector('[name="firstName"]').value = user.firstName
+    form.querySelector('[name="lastName"]').value = user.lastName
+    form.querySelector('[name="balance"]').value = user.balance
+    form.querySelector('[name="isActive"]').checked = user.isActive
+
+    loadedUser = user
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element
+ * @param {(userLike)=> Promise<void>} callback
+ */
+export const renderModal = (element, callback) => {
 
     if (modal) return
 
@@ -34,11 +58,11 @@ export const renderModal = (element) => {
         hideModal()
     })
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
         event.preventDefault()
 
         const formData = new FormData(form)
-        const userLike = {}
+        const userLike = { ...loadedUser }
 
         for (const [key, value] of formData) {
             if (key === 'balance') {
@@ -46,18 +70,19 @@ export const renderModal = (element) => {
                 continue
             }
 
+            userLike[key] = value
+
             if (key === 'isActive') {
-                userLike[key] = (value === 'on') ? true : false
+                userLike[key] = true
                 continue
             }
-
-
-            userLike[key] = value
         }
 
+        if (!userLike.hasOwnProperty('isActive')) userLike.isActive = false
 
-        // TODO: salvar usuario
+        await callback(userLike)
 
+        hideModal()
     })
 
     element.append(modal)
