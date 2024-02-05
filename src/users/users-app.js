44b94edada3bb8bcd1f8110f0e4212fbcc1bc5c7
Andrés Aristizábal <andres.aristizabal@gmail.com>
2024-02-04 20:23:54 -0500

    feat: add table, pagination and button to DOM

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,4 +1,7 @@
-import userStore from "./store/user-store"
+import { renderAddButton } from "./presentation/render-add-button/render-add-button"
+import { renderButtons } from "./presentation/render-buttons/render-buttons"
+import { renderTable } from "./presentation/render-table/render-table"
+import usersStore from "./store/users-store"
 
 /**
  * 
@@ -6,5 +9,10 @@ import userStore from "./store/user-store"
  */
 export const UserApp = async (element) => {
     element.innerHTML = 'Loading...'
-    await userStore.loadNextPage()
+    await usersStore.loadNextPage()
+
+    element.innerHTML = ''
+    renderTable(element)
+    renderButtons(element)
+    renderAddButton(element)
 }
